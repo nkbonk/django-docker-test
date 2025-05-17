30b95f98b74b64afa6c83f68c57d18c9965bb0f7
nkbonk <nebonk846@gmail.com>
2025-05-17 16:30:18 +0500

    добавил hello API

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from main.api_views import hello_api
 from django.urls import path, include  # ⬅ добавили include
 
 urlpatterns = [
+    path('api/hello/', hello_api),
     path('admin/', admin.site.urls),
     path('', include('main.urls')),  # ⬅ подключили маршруты из приложения
 ]
